cd271e82ce8b986f42ae5b7330956c19e1f7a8c6
Alan Tang <giabao_tang@yahoo.com>
2020-10-10 23:41:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,4 +21,8 @@ class Artist
   def self.find_or_create_by_name(name)
     all.find{|artist| artist.name == name} || Artist.new(name)
   end
+  
+  def print_songs 
+    songs.each{|song| puts song}
+  end
 end 
