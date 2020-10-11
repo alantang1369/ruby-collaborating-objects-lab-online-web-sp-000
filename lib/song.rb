5619a80d57ec4f442988cbc1782e98d63bc5d51d
Alan Tang <giabao_tang@yahoo.com>
2020-10-11 01:03:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   def self.new_by_filename(file)
       array = file.split(" - ")
       song = Song.new(array[1])
-      song.artist = Artist.new(array[0])
+      #song.artist = Artist.new(array[0])
     
   end  
 end
