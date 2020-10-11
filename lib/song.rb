d47b563c396ca663705a72d05d5a3588c5250d00
Alan Tang <giabao_tang@yahoo.com>
2020-10-11 01:31:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,8 @@ class Song
     song
   end  
   
-  def artist_name=(name, song)
+  def artist_name=(name)
    artist = Artist.find_or_create_by_name(name)
-   artist.add_song
+   artist.add_song(self)
   end  
 end
